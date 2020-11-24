185ef86b3cd83bbd20fda246e46fbbae40434a84
Arvind Kumar Mahto <arvind.kumar8271043313@gmail.com>
2020-11-24 09:24:48 +0530

    refactoring for MVC

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,17 +2,12 @@ const express = require('express');
 const tourController = require('./../controllers/tourController');
 const router = express.Router();
 
-router.param('id', tourController.checkID);
-
-//create a checkBody middleware
-//check if body contains the name and price property
-//if not ,send back 400 (bad request)
-//add it to the post handler stack
+// router.param('id', tourController.checkID);
 
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.checkBody, tourController.createTour); //added middleware function(checkBody) , affter middleware only tourcontroller.createTour will run
+  .post(tourController.checkBody, tourController.createTour);
 
 router
   .route('/:id')
--- server.js
+++ server.js
@@ -18,38 +18,6 @@ mongoose
   })
   .then(() => console.log('DB connection successfull'));
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-    unique: true,
-  },
-});
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Park Camper',
-  price: 997,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log('ERROR ✴️:', err);
-  });
-
 const port = process.env.port || 3000;
 app.listen(port, () => {
   console.log(`app running on port ${port}.......`);
